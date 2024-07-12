8bb64272840a751f6ddb683eb376d5ef04be11da
LearnWithSamiran <102744660+sam-iran@users.noreply.github.com>
2024-07-12 08:01:17 +0530

    Create App.jsx

---
+++ ToDo_List/src/App.jsx
@@ -0,0 +1,18 @@
+import './App.css'
+import ToDo from "../components/ToDo"
+import ToDoList from '../components/ToDoList'
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <h2>To Do List</h2>
+        <ToDo/>
+        <ToDoList/>
+      </div>
+    </>
+  )
+}
+
+export default App

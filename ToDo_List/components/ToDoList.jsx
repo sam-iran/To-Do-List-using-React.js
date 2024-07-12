85a49ca20861059f006d6a62205ff8285f69994d
LearnWithSamiran <102744660+sam-iran@users.noreply.github.com>
2024-07-12 07:59:48 +0530

    Add files via upload

---
+++ ToDo_List/components/ToDoList.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function ToDoList() {
+  return (
+    <div className='list_container'>
+        <div className='info'>
+            <p>Go to gym</p>
+            <button>Delete</button>
+        </div>
+    </div>
+  )
+}
+
+export default ToDoList

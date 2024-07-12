8ab982a73273f81d3181495b0fd12d214b45bfb7
LearnWithSamiran <102744660+sam-iran@users.noreply.github.com>
2024-07-12 07:59:25 +0530

    Create ToDo.jsx

---
+++ ToDo_List/components/ToDo.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function ToDo() {
+  return (
+        <div className='input_container'>
+            <input type='text' placeholder='Enter a Task'></input>
+            <button>Add</button>
+        </div>
+  )
+}
+
+export default ToDo
